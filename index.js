ac1fe55fb0631523e3c3393f93e95d36925c75b4
Saad Ahmad <saad.shayansolutions@gmail.com>
2023-02-08 17:47:28 +0500

    Cards completed-01

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-let newCard='';
-async function fetchText() {
+let newCard=``;
+let cardEL=document.querySelector(".countries-box");
+const fetchText=async ()=> {
+    try{
     let response = await fetch('https://restcountries.com/v3.1/all');
-    return await response.json();
-}
+ let data =await response.json();
 
-
-fetchText().catch(console.error("Api Error")).
-then((data)=>{
     data.forEach((element) => {
         // console.log(`Country Name:${element.name.common}`);
         // console.log(`Country population:${element.population}`);
         // console.log(`Country Capital:${element.capital}`);
         // console.log(`Country Region:${element.region}`);
         // console.log(`Country flag:${element.flags.png}`);
-        newCard+=`<div class="card"><img src="${element.flags.png}" alt="flag">
+        newCard+=`
+        <div class="country-item">
+        <div class="flag"> 
+        <img src="${element.flags.png}" alt="flag" width="270" height="160">
+        </div>
+        <div class="description">
         <h3><span>${element.name.common}</span></h3>
-        <p><span>Population: </span>${element.population}</p>
+        <p id="pop"><span>Population: </span>${element.population}</p>
         <p><span>Region: </span>${element.region}</p>
-        <p><span>Capital: </span>${element.capital}</p></div>`;
-    
+        <p><span>Capital: </span>${element.capital}</p>
+        </div>
+        </div>`;
     });
-    var cardEL=document.querySelector("#cards")
     cardEL.innerHTML=`${newCard}`;
-})
+}
+   catch(error){console.log(error)}
+}
+
+
+fetchText();
+function toggleTheme() {
+    
+    var currentTheme = document.documentElement.getAttribute('data-theme');
+    var text = document.getElementById('dark-mode-text');
+
+    if (currentTheme === 'light') {
+        targetTheme = 'dark';
+        text.innerText = "Light Mode";
+        document.getElementsByTagName('ion-icon')[0].setAttribute('name', 'sunny-outline');
+    } else if (currentTheme === 'dark') {
+        targetTheme = 'light';
+        text.innerText = "Dark Mode";
+        document.getElementsByTagName('ion-icon')[0].setAttribute('name', 'moon-outline');
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme);
+
+}
