b20d303d59a061cb9a4fb4f0a6204f95e569b3bd
Saad Ahmad <saad.shayansolutions@gmail.com>
2023-02-13 15:02:46 +0500

    updated

--- details.js
+++ details.js
@@ -1,79 +1,97 @@
-
-let cardEL=document.querySelector(".countries-box");
-const fetchText=async ()=> {
-    try{
-    var countryName=sessionStorage.getItem("countryName");
-    let response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+let cardEL = document.querySelector(".countries-box");
+const fetchText = async () => {
+  try {
+    var countryName = sessionStorage.getItem("countryName");
+    let response = await fetch(
+      `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
+    );
     let data = await response.json();
 
     data.forEach((element) => {
-        let newCard=document.createElement("div");
-        newCard.classList.add("text-grid");
-        newCard.innerHTML=
-        `<div class="details-flag">
+      let newCard = document.createElement("div");
+      newCard.classList.add("text-grid");
+      newCard.innerHTML = `<div class="details-flag">
         <img src=${element.flags.png} alt="flag image">
         </div>
         <div class="description2">
-        <div >
-            <h3>${element.name.common}</h3>
+            <h2><span>${element.name.common}</span></h2>
             <p > <span>Native Name:</span> ${
               Object.values(element.name.nativeName)[0].common ||
               element.name.nativeName
             }</p>
-            <p><span>Population:</span> ${element.population}</p>
+            <p><span>Population:</span> ${element.population.toLocaleString()}</p>
             <p><span>Region:</span> ${element.region}</p>
             <p><span>Sub Region:</span> ${element.subregion}</p>
             <p><span>Capital:</span> ${element.capital}</p>
-       </div>
-       <div>
             <p><span>Top Level Domain:</span> ${element.tld}</p>
-            <p><span>Currencies:</span> ${Object.values(element.currencies)[0].name}</p>
-            <p><span>Languages: </span> ${Object.values(element.languages)[0]}</p>
-       </div>
+            <p><span>Currencies:</span> ${
+              Object.values(element.currencies)[0].name
+            }</p>
+            <p><span>Languages: </span> ${
+              Object.values(element.languages)[0]
+            }</p>
        
-       <div>
-           <p class='border-countries-box'><span>Border Countries:</span> ${element.borders === undefined ? "No borders" : element.borders}
+       <div class="neighboring-countries">
+           <div class="border-countries-box">
+           <p><span>Border Countries:</span> ${
+             element.borders === undefined ? "No borders" : element.borders
+           }
            </p>
+           </div>
        </div>
-        </div>`;
-        cardEL.appendChild(newCard);
+       </div>
+     `;
+      cardEL.appendChild(newCard);
     });
-}
-   catch(error){console.log(error)}
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const toggleEl=document.querySelector(".theme-box");
+const toggleEl = document.querySelector(".theme-box");
 
-toggleEl.addEventListener("click",()=> {
-    
-    var currentTheme = document.documentElement.getAttribute('data-theme');
-    var text = document.getElementById('dark-mode-text');
+toggleEl.addEventListener("click", () => {
+  let currentTheme = document.documentElement.getAttribute("data-theme");
+  let targetTheme = "";
+  let text = document.getElementById("dark-mode-text");
 
-    if (currentTheme === 'light') {
-        targetTheme = 'dark';
-        text.innerText = "Light Mode";
-        document.getElementsByTagName('ion-icon')[0].setAttribute('name', 'sunny-outline');
-    } else if (currentTheme === 'dark') {
-        targetTheme = 'light';
-        text.innerText = "Dark Mode";
-        document.getElementsByTagName('ion-icon')[0].setAttribute('name', 'moon-outline');
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+    text.innerText = "Light Mode";
+    document
+      .getElementsByTagName("ion-icon")[0]
+      .setAttribute("name", "sunny-outline");
+  } else if (currentTheme === "dark") {
+    targetTheme = "light";
+    text.innerText = "Dark Mode";
+    document
+      .getElementsByTagName("ion-icon")[0]
+      .setAttribute("name", "moon-outline");
+  }
+
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+});
+
+window.addEventListener("load", () => {
+  let theme = localStorage.getItem("theme");
+  if (theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+    let text = document.getElementById("dark-mode-text");
+    if (theme === "light") {
+      text.innerText = "Dark Mode";
+      document
+        .getElementsByTagName("ion-icon")[0]
+        .setAttribute("name", "moon-outline");
+    } else {
+      text.innerText = "Light Mode";
+      document
+        .getElementsByTagName("ion-icon")[0]
+        .setAttribute("name", "sunny-outline");
     }
-
-    document.documentElement.setAttribute('data-theme', targetTheme);
-
-})
-
+  }
+});
 
 //Function calling
 
 fetchText();
-
-
-
-
-
-
-
-
-
-
--- index.js
+++ index.js
@@ -1,81 +1,99 @@
-
-const cardEL=document.querySelector(".countries-box");
+const cardEL = document.querySelector(".countries-box");
 const backButton = document.getElementById("back-btn");
 const filter = document.getElementById("regions");
 const form = document.getElementById("input-form");
 const searchInput = document.getElementById("search");
 
+const showData = () => {
+  let url = "https://restcountries.com/v3.1/all";
+  fetchText(url);
+};
 
-const showData=()=>{
-let url='https://restcountries.com/v3.1/all';
-fetchText(url);
-}
-
-
-const fetchText=async (url)=> {
-    try{
+const fetchText = async (url) => {
+  try {
     let response = await fetch(url);
-     let data =await response.json();
+    let data = await response.json();
 
     data.forEach((element) => {
-        let newCard=document.createElement("div");
-        newCard.classList.add("card");
-        newCard.innerHTML=
-        `<div class="country-item">
+      let newCard = document.createElement("div");
+      newCard.classList.add("card");
+      newCard.innerHTML = `<div class="country-item">
         <div class="flag"> 
-        <img src="${element.flags.png}" alt="flag" width="288" height="175">
+        <img src="${element.flags.png}" alt="flag" width="288" height="170">
         </div>
         <div class="description">
         <h3><span>${element.name.common}</span></h3>
-        <p id="pop"><span>Population: </span>${element.population}</p>
+        <p><span>Population: </span>${element.population}</p>
         <p><span>Region: </span>${element.region}</p>
         <p><span>Capital: </span>${element.capital}</p>
         </div>
         </div>
         `;
 
-        newCard.addEventListener("click",()=> {
-        window.location.assign("./details.html");
-         sessionStorage.setItem("countryName",element.name.common);
-        });
-        cardEL.appendChild(newCard);
+      newCard.addEventListener("click", () => {
+        window.location.assign(
+          "./details.html"
+        );
+        sessionStorage.setItem("countryName", element.name.common);
+      });
+      cardEL.appendChild(newCard);
     });
-    
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
-   catch(error){console.log(error)}
-}
+const toggleEl = document.querySelector(".theme-box");
 
+toggleEl.addEventListener("click", () => {
+  let currentTheme = document.documentElement.getAttribute("data-theme");
+  let targetTheme = "";
+  let text = document.getElementById("dark-mode-text");
 
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+    text.innerText = "Light Mode";
+    document
+      .getElementsByTagName("ion-icon")[0]
+      .setAttribute("name", "sunny-outline");
+  } else if (currentTheme === "dark") {
+    targetTheme = "light";
+    text.innerText = "Dark Mode";
+    document
+      .getElementsByTagName("ion-icon")[0]
+      .setAttribute("name", "moon-outline");
+  }
 
-const toggleEl=document.querySelector(".theme-box");
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+});
 
-toggleEl.addEventListener("click",()=> {
-    
-    let currentTheme = document.documentElement.getAttribute('data-theme');
-    let text = document.getElementById('dark-mode-text');
-
-    if (currentTheme === 'light') {
-        targetTheme = 'dark';
-        text.innerText = "Light Mode";
-        document.getElementsByTagName('ion-icon')[0].setAttribute('name', 'sunny-outline');
-    } else if (currentTheme === 'dark') {
-        targetTheme = 'light';
-        text.innerText = "Dark Mode";
-        document.getElementsByTagName('ion-icon')[0].setAttribute('name', 'moon-outline');
+window.addEventListener("load", () => {
+  let theme = localStorage.getItem("theme");
+  if (theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+    let text = document.getElementById("dark-mode-text");
+    if (theme === "light") {
+      text.innerText = "Dark Mode";
+      document
+        .getElementsByTagName("ion-icon")[0]
+        .setAttribute("name", "moon-outline");
+    } else {
+      text.innerText = "Light Mode";
+      document
+        .getElementsByTagName("ion-icon")[0]
+        .setAttribute("name", "sunny-outline");
     }
-
-    document.documentElement.setAttribute('data-theme', targetTheme);
-
-})
-
-
+  }
+});
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const searchTerm = searchInput.value;
-  const response = await fetch(`https://restcountries.com/v2/name/${searchTerm}`);
+  const response = await fetch(
+    `https://restcountries.com/v2/name/${searchTerm}`
+  );
   const data = await response.json();
 
   cardEL.innerHTML = "";
@@ -86,14 +104,13 @@ form.addEventListener("submit", async (event) => {
     const flagElement = document.createElement("img");
     flagElement.classList.add("flag");
     flagElement.src = country.flag;
-
+    
     const titleElement = document.createElement("h3");
     titleElement.classList.add("headingEl");
     titleElement.textContent = country.name;
-
     const listElement = document.createElement("ul");
     listElement.innerHTML = `
-      <li><span>Population: </span>${country.population}</li>
+      <li><span>Population: </span>${country.population.toLocaleString()}</li>
       <li><span>Region: </span>${country.region}</li>
       <li><span>Capital: </span>${country.capital}</li>
     `;
@@ -104,8 +121,6 @@ form.addEventListener("submit", async (event) => {
     cardEL.appendChild(countryElement);
   }
 });
-
-
 
 const filterByRegion = async () => {
   let Url = "https://restcountries.com/v3.1/all";
@@ -122,23 +137,19 @@ const filterByRegion = async () => {
   });
 };
 
- 
-  filter.addEventListener("change", (e) => {
-    let SearchName=e.target.value;
-    if (SearchName === "") return;
-    cardEL.innerHTML = "";
-    const Url = `https://restcountries.com/v3.1/region/${SearchName}`;
-    fetchText(Url);
-  });
 
+
+
+
+
+filter.addEventListener("change", (e) => {
+  let SearchName = e.target.value;
+  if (SearchName === "") return;
+  cardEL.innerHTML = "";
+  const Url = `https://restcountries.com/v3.1/region/${SearchName}?fullText=true`;
+  fetchText(Url);
+});
 
 showData();
 filterByRegion();
-
-
-
-
-
-
-
 
